eaf7da18dbcf968c99d58262ffa7bf963ff8050e
Mouniesh <iammounieshv@gmail.com>
2024-08-16 22:44:45 +0530

    register functionality is added

--- auth-tut/actions/register.js
+++ auth-tut/actions/register.js
@@ -1,18 +1,40 @@
-"use server"
+"use server";
 
-import { RegisterSchema } from "@/schemas"
+import { db } from "@/lib/db";
+import bcrypt from "bcryptjs";
+import { RegisterSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
 
 const register = async (values) => {
-    const validateFields = RegisterSchema.safeParse(values);
+  const validateFields = RegisterSchema.safeParse(values);
 
-    if(!validateFields.success){
-        return {
-            error : "Invalid fields"
-        }
-    }
-    return{ 
-        success : 'Email sent'
-    }
-}
+  if (!validateFields.success) {
+    return {
+      error: "Invalid fields",
+    };
+  }
 
-export default register
+  const { email, password, name } = validateFields.data;
+  const hashedPass = await bcrypt.hash(password, 10);
+
+  const existEmail = await getUserByEmail(email)
+  if (existEmail) {
+    return {
+      error: "Email already exists",
+    };
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPass,
+    },
+  });
+
+  return {
+    success: "User Created",
+  };
+};
+
+export default register;
---
+++ auth-tut/data/user.js
@@ -0,0 +1,21 @@
+const { db } = require("@/lib/db");
+
+export const getUserByEmail  = async (email) => {
+    try {
+        const user = await db.user.findUnique({where : {email}});
+
+        return user
+    } catch {
+        return null
+    }
+}
+
+export const getUserById  = async (id) => {
+    try {
+        const user = await db.user.findUnique({where : {id}});
+
+        return user
+    } catch {
+        return null
+    }
+}
