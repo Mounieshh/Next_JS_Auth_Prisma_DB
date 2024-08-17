5bc9b8ed480bc55625484ff6b74487014cfa82e8
Mouniesh <iammounieshv@gmail.com>
2024-08-17 15:34:04 +0530

    middleware file added

---
+++ auth-tut/middleware.js
@@ -0,0 +1,19 @@
+import { auth } from "@/auth"
+ 
+export default auth((req) => {
+    const isLogged = !!req.auth
+  console.log("ROUTE : " , req.nextUrl.pathname);
+  console.log("LOG : " , isLogged);
+  
+  
+})
+ 
+
+export const config = {
+    matcher: [
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
+      ],
+}
